a166a16c8e0965dbbdda06bb46f27de116189315
Muhammad Osama <muhammad.osama@codezac.com>
2026-01-22 00:05:44 +0500

    Added

---
+++ backend/prisma.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'prisma/config'
+
+export default defineConfig({
+  schema: './prisma/schema.prisma',
+  migrations: {
+    seed: 'npx tsx src/prisma/seed.ts',
+  },
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+})
